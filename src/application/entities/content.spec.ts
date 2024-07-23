0c4be587a3ef3804353084352fd87329217122d1
anderson sfiems <kardek.anderson@gmail.com>
2024-07-23 15:15:36 -0400

    feat: add test notification

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,17 +1,17 @@
 import { Content } from './content';
 
-describe('Test content', () => {
-  it('Should be able to create a notification content', () => {
+describe('Notification content', () => {
+  it('should be able to create a notification content', () => {
     const content = new Content('Você passou no teste');
 
     expect(content).toBeTruthy();
   });
 
-  it('Should not be able to create a notification content if less than 5 characteres', () => {
+  it('should not be able to create a notification content if less than 5 characteres', () => {
     expect(() => new Content('aaa')).toThrow;
   });
 
-  it('Should not be able to create a notification content if more than 240 characteres', () => {
+  it('should not be able to create a notification content if more than 240 characteres', () => {
     expect(() => new Content('a'.repeat(241))).toThrow;
   });
 });
