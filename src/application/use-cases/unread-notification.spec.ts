d048c3e225b996bd641ece061b895c76bd65001e
anderson sfiems <kardek.anderson@gmail.com>
2024-07-29 10:49:00 -0400

    feat: read-notification and unread-notification

---
+++ src/application/use-cases/unread-notification.spec.ts
@@ -0,0 +1,39 @@
+import { InMemoryNotificationsRepository } from '../../../test/repositories/in-memory-notifications-repositorie';
+import { Content } from '../entities/content';
+import { Notification } from '../entities/notification';
+
+import { NotificationNotFound } from './errors/notification-not-found';
+import { UnreadNotification } from './unread-notification';
+
+describe('Unread notification', () => {
+  it('should be able to Unread a notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    const notification = new Notification({
+      category: 'social',
+      content: new Content('Nova solicitação de amizade'),
+      recipientId: 'example-recipient-id',
+      readAt: new Date(),
+    });
+
+    await notificationsRepository.create(notification);
+
+    await unreadNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].readAt).toBeNull();
+  });
+
+  it('should not be able to unread a non existing notification', async () => {
+    const notificationsRepository = new InMemoryNotificationsRepository();
+    const unreadNotification = new UnreadNotification(notificationsRepository);
+
+    expect(() => {
+      return unreadNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
