d82825bfa49858fd0c121bdfe326439a8a6869b2
anderson sfiems <kardek.anderson@gmail.com>
2024-07-29 15:46:05 -0400

    feat: finish routes

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
-import { NotificationsController } from './controllers/notifications.controller';
+import { CancelNotification } from 'src/application/use-cases/cancel-notification';
+import { CountRecipientNotifications } from 'src/application/use-cases/count-recipient-notifications';
+import { GetRecipientNotifications } from 'src/application/use-cases/get-recipient-notifications';
+import { ReadNotification } from 'src/application/use-cases/read-notification';
 import { SendNotification } from 'src/application/use-cases/send-notification';
+import { UnreadNotification } from 'src/application/use-cases/unread-notification';
 import { DatabseModule } from '../database/database.module';
+import { NotificationsController } from './controllers/notifications.controller';
 
 @Module({
   imports: [DatabseModule],
   controllers: [NotificationsController],
-  providers: [SendNotification],
+  providers: [
+    SendNotification,
+    CancelNotification,
+    CountRecipientNotifications,
+    GetRecipientNotifications,
+    ReadNotification,
+    UnreadNotification,
+  ],
 })
 export class HttpModule {}
